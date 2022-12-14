e729b86c35fe7c35b44a180140655b027d1303f5
carlosG06 <74464196+carlosG06@users.noreply.github.com>
2022-12-14 19:00:25 +0100

    Delete redundant import from HeroFavInfoActivity.kt

--- app/src/main/java/es/unex/trackstone10/HeroFavInfoActivity.kt
+++ app/src/main/java/es/unex/trackstone10/HeroFavInfoActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import es.unex.trackstone10.AppExecutors
 import es.unex.trackstone10.ButtonNavigationMenuActivity
 import es.unex.trackstone10.databinding.ActivityHeroeSkinInfoBinding
 import es.unex.trackstone10.roomdb.Entity.ClassEntity
